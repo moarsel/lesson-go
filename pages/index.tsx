46d99bd13614da3beadda6fbbd872f815857b37b
Russell <russell.hoy@gmail.com>
2023-02-07 21:50:55 -0800

    typing effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import TypeWriterEffect from "typewriter-effect";
 
 const Home: NextPage = () => {
+  const headingRef = useRef<HTMLHeadingElement>(null);
+  const [loaded, setLoaded] = useState(false);
+  const isBrowser = typeof window !== "undefined";
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 mx-auto">
       <Head>
@@ -14,8 +23,27 @@ const Home: NextPage = () => {
 
       <Header />
       <main className="flex flex-col items-center flex-1 w-full px-4 mt-20 text-center sm:mt-24">
-        <h1 className="max-w-2xl px-4 text-4xl font-bold sm:text-5xl text-slate-900">
-          Create superb lesson plans with superhuman speed 🤖
+        <h1
+          ref={headingRef}
+          aria-label="Create superb lesson plans with superhuman speed"
+          className="max-w-2xl px-4 text-4xl font-bold sm:text-5xl text-slate-900"
+        >
+          {loaded && (
+            <TypeWriterEffect
+              options={{ delay: 90, autoStart: false }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Create superb lesson plans")
+                  .pauseFor(1200)
+                  .changeDelay(15)
+                  .typeString(" with superhuman speed 🤖")
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .start();
+              }}
+            ></TypeWriterEffect>
+          )}
         </h1>
         <p className="mt-5 text-lg sm:text-2xl text-slate-700">
           Go from no ideas to polished lesson plan in a snap!
